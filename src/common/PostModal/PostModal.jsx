93f8365f84044f640f3872f9827a20e7918ae1e0
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-04-08 11:46:11 +0200

    Feat: added react modal to new post function

---
+++ src/common/PostModal/PostModal.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react"
+import Modal from "react-modal"
+import "./PostModal.css"
+import { CustomButton } from "../CustomButton/CustomButton"
+import { CustomInputTextArea } from "../CustomInputTextArea/CustomInputTextArea"
+const PostModal = ({
+  thePost,
+  newPost,
+  setNewPost,
+  postChanged,
+  setPostChanged,
+  editMyPost,
+  inputHandler,
+}) => {
+  const [modalIsOpen, setIsOpen] = useState(false)
+
+  Modal.setAppElement("#root")
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+  console.log(thePost._id)
+  return (
+    <div className="postModalContainter">
+      <CustomButton
+        className={" addAppointment"}
+        title={"Edit Post"}
+        functionEmit={openModal}
+      />
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Modal"
+        className="Modal"
+        overlayClassName="Overlay"
+      >
+        <CustomButton
+          className={"closeModalButton"}
+          title={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-x-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+            </svg>
+          }
+          functionEmit={closeModal}
+        />
+        <div className="bodyPostModal">
+          <label className="labelTextArea">Edit your post</label>
+          <CustomInputTextArea
+            className=" textarea "
+            type={"text"}
+            name={"content"} //must be the key name idem like state newPost
+            disabled={""}
+            value={newPost.content || ""}
+            placeholder="write your post...."
+            functionChange={(e) => inputHandler(e)}
+            // onBlurFunction={(e) => checkError(e)}
+          />
+          <CustomButton
+            className={"primaryButton"}
+            title={"Edit Post"}
+            functionEmit={() => editMyPost(thePost._id)}
+          />
+        </div>
+      </Modal>
+    </div>
+  )
+}
+
+export default PostModal
--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -17,10 +17,13 @@ import { CustomInputTextArea } from "../../common/CustomInputTextArea/CustomInpu
 
 import { ToastContainer, toast } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import PostModal from "../../common/PostModal/PostModal"
 
 const Post = () => {
   const [myPosts, setMyPosts] = useState()
   const [postChanged, setPostChanged] = useState(false)
+  const [isOpenAddPost, setIsOpenAddPost] = useState(false)
+
   const [newPost, setNewPost] = useState({
     content: "",
   })
@@ -146,105 +149,149 @@ const Post = () => {
         <div className="postDesign">
           <div className="myPostContainer">
             <div className="titlePostContainer">My Posts / date:</div>
-            <div className="postList">
-              {myPosts?.map((element) => (
-                <div className="post" key={element._id}>
-                  <div className="date">
-                    {new Date(element.createdAt)
-                      .toDateString()
-                      .replace("2024", " ")}
-                    {/* poner algoritmo q diga q si es otro año lo ponga sino vacio */}
-                    {/* puedo hacer lo mismo con post de hoy o de ayer */}
-                  </div>
-                  <div className="postContent">{element.content}</div>
-
-                  <div className="iconsInteraction">
-                    <div className="likesElements">
-                      <CustomButton
-                        className="likes"
-                        title={
-                          <>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="12"
-                              height="12"
-                              fill="currentColor"
-                              className="bi bi-heart-fill"
-                              viewBox="0 0 16 16"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
-                              />
-                            </svg>{" "}
-                          </>
-                        }
-                        functionEmit={() => likesPost(element._id)}
-                      ></CustomButton>{" "}
-                      {element.likes.length}
-                    </div>
-                    <CustomButton
-                      className={"primaryButton"}
-                      title={"Delete Post"}
-                      functionEmit={() => deleteMyPost(element._id)}
-                    />
-                    <div className="comments">
-                      <CustomButton
-                        className="message"
-                        title={
-                          <>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="12"
-                              height="12"
-                              fill="currentColor"
-                              className="bi bi-chat"
-                              viewBox="0 0 16 16"
-                            >
-                              <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
-                            </svg>{" "}
-                            0
-                          </>
-                        }
-                      />
-                      <label className="labelTextArea">Edit your post</label>
-                      <CustomInputTextArea
-                        className=" textarea "
-                        type={"text"}
-                        name={"content"} //must be the key name idem like state newPost
-                        disabled={""}
-                        value={newPost.content || ""}
-                        placeholder="write your post...."
-                        functionChange={(e) => inputHandler(e)}
-                        // onBlurFunction={(e) => checkError(e)}
-                      />
-                      <CustomButton
-                        className={"primaryButton"}
-                        title={"Edit Post"}
-                        functionEmit={() => editMyPost(element._id)}
-                      />
-                    </div>
-                  </div>
+            <CustomButton
+              className={"addAppointment"}
+              title={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
+                  fill="currentColor"
+                  className="bi bi-plus-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                </svg>
+              }
+              functionEmit={() => setIsOpenAddPost(!isOpenAddPost)}
+            />
+            {isOpenAddPost && (
+              <div className="newPost">
+                <CustomInputTextArea
+                  className=" textarea "
+                  type={"text"}
+                  name={"content"} //must be the key name idem like state newPost
+                  disabled={""}
+                  value={newPost.content || ""}
+                  placeholder="write your post...."
+                  functionChange={(e) => inputHandler(e)}
+                  // onBlurFunction={(e) => checkError(e)}
+                />
+                <div className="modalButtons">
+                  <CustomButton
+                    className={"cancelModalButton"}
+                    title={
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="22"
+                        height="22"
+                        fill="currentColor"
+                        className="bi bi-x-circle"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                      </svg>
+                    }
+                    functionEmit={() => setIsOpenAddPost(!isOpenAddPost)}
+                  />
+                  <CustomButton
+                    className={"sendModalButton"}
+                    title={
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="22"
+                        height="22"
+                        fill="currentColor"
+                        className="bi bi-send"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
+                      </svg>
+                    }
+                    functionEmit={() => sendNewPost()}
+                  />
                 </div>
-              ))}
-            </div>
+              </div>
+            )}
+            {myNewPost && (
+              <div className="postList">
+                {myPosts?.map((element) => (
+                  <div className="post" key={element._id}>
+                    <div className="date">
+                      {new Date(element.createdAt)
+                        .toDateString()
+                        .replace("2024", " ")}
+                      {/* poner algoritmo q diga q si es otro año lo ponga sino vacio */}
+                      {/* puedo hacer lo mismo con post de hoy o de ayer */}
+                    </div>
+                    <div className="postContent">{element.content}</div>
+                    <PostModal
+                      thePost={element}
+                      newPost={newPost}
+                      setNewPost={setNewPost}
+                      postChanged={postChanged}
+                      setPostChanged={setPostChanged}
+                      inputHandler={inputHandler}
+                      editMyPost={editMyPost}
+                    />
+                    <div className="iconsInteraction">
+                      <div className="likesElements">
+                        <CustomButton
+                          className="likes"
+                          title={
+                            <>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="12"
+                                height="12"
+                                fill="currentColor"
+                                className="bi bi-heart-fill"
+                                viewBox="0 0 16 16"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+                                />
+                              </svg>{" "}
+                            </>
+                          }
+                          functionEmit={() => likesPost(element._id)}
+                        ></CustomButton>{" "}
+                        {element.likes.length}
+                      </div>
+                      <CustomButton
+                        className={"closeModalButton"}
+                        title={"Delete Post"}
+                        functionEmit={() => deleteMyPost(element._id)}
+                      />
+                      <div className="comments">
+                        <CustomButton
+                          className="message"
+                          title={
+                            <>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="12"
+                                height="12"
+                                fill="currentColor"
+                                className="bi bi-chat"
+                                viewBox="0 0 16 16"
+                              >
+                                <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
+                              </svg>{" "}
+                              0
+                            </>
+                          }
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
-          <label className="labelTextArea">Insert your post</label>
-          <CustomInputTextArea
-            className=" textarea "
-            type={"text"}
-            name={"content"} //must be the key name idem like state newPost
-            disabled={""}
-            value={newPost.content || ""}
-            placeholder="write your post...."
-            functionChange={(e) => inputHandler(e)}
-            // onBlurFunction={(e) => checkError(e)}
-          />
-          <CustomButton
-            className={"primaryButton"}
-            title={"Send Post"}
-            functionEmit={() => sendNewPost()}
-          />
         </div>
       </>
       <ToastContainer />
